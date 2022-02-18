45038d0d21c782586928c37ba74cf092d658d0a9
Nehal Ingole <nehal712521@gmail.com>
2022-02-18 21:31:46 +0530

    First commit

---
+++ Python GCEK/ArithmaticOP.py
@@ -0,0 +1,13 @@
+#power operator 
+print(5**2)
+#shortcut 
+i=5
+i=i+2
+i +=2
+# Operator precendence 
+result = 2 + 3 * 5  #bodmassp
+print(result)
+#comparison  True or  False
+print(2>3)
+
+
---
+++ Python GCEK/variable.py
@@ -0,0 +1,36 @@
+# how to take a input from user concantation 
+name = input("What is your name ?")
+print ("Hello " + name)
+
+# Type Conversion 
+#/ python input is came  into the string format  number , floating , boolean value 
+
+old_age = input("Enter your old : ")
+
+new_age = int(old_age) +2  # int will take age as a number 
+print(new_age)
+
+# float () , str(),bool()
+
+# print the sum of two number 
+
+first = input("Enter the first number ")
+second = input("Enter the second number ")
+
+sum = int(first) + int(second)
+print(sum)
+
+
+#string  all not zhed zad with original string
+name = "Nehal Ingole "
+name.upper()   
+
+#find operation
+print(name.find('e'))
+
+#replace 
+print(name.replace("Nehal" , "NN"))
+
+#find the single word from string and variable 
+print("Nehal" in name )
+
